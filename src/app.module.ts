edde8d177bf185b9426b1d4d6617f087b541194b
mitchel <edahmitchel@example.com>
2022-09-30 00:17:47 +0100

    ENV

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { RoleController } from './user/controllers/role/role.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController],
+  controllers: [AppController, UserController, RoleController],
   providers: [AppService, UserService],
 })
 export class AppModule {}
